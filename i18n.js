41cfd7d6d0e17234b86782427cf54f9fbd001aa2
0xbatutut <long.huynh+batutut@autonomous.nyc>
2018-06-22 08:45:54 +0700

    Update CI

--- i18n.js
+++ i18n.js
@@ -1,11 +1,11 @@
 
-const de = require('./i18n/de');
-const es = require('./i18n/es');
-const fr = require('./i18n/fr');
-const ja = require('./i18n/ja');
-const ko = require('./i18n/ko');
-const ru = require('./i18n/ru');
-const zh = require('./i18n/zh');
+const de = require('./i18n/de.json');
+const es = require('./i18n/es.json');
+const fr = require('./i18n/fr.json');
+const ja = require('./i18n/ja.json');
+const ko = require('./i18n/ko.json');
+const ru = require('./i18n/ru.json');
+const zh = require('./i18n/zh.json');
 
 module.exports = {
   de, es, fr, ja, ko, ru, zh,
